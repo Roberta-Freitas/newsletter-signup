7306b9e38b4997cf396b07deaad6e6405d084c4c
RobertaFreitas <robertaefreitas@gmail.com>
2023-06-12 21:14:02 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const https = require('https');
+
+
+const app = express();
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.get("/success.html", function(req, res){
+  res.sendFile(__dirname + "/success.html");
+});
+
+app.get("/failure.html", function(req, res){
+  res.sendFile(__dirname + "/failure.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  }
+
+  const jasonData = JSON.stringify(data);
+
+  const url = "https://us13.api.mailchimp.com/3.0/lists/c284beb6e7"
+
+  const options = {
+    method: "POST",
+    auth: "roberta11:82b4a8b99f5403cc8cc858dcc155f92a-us13"
+  }
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function(data){
+      const responseData = JSON.parse(data);
+      if (response.statusCode === 200) {
+        res.redirect("/success.html");
+      } else {
+        res.redirect("/failure.html");
+      }
+    })
+})
+  request.write(jasonData);
+  request.end();
+})
+
+
+
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Running on localhost 3000");
+  })
